e70f424a0d0b7db8683df48da7b02d5766aed53f
Fayzullo Nasibullaev <42410147+nasibullaev@users.noreply.github.com>
2026-06-29 19:26:09 +0500

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+
+    if (s.length !== t.length) {
+
+        return false;
+
+    }
+
+    const count = new Map();
+
+    for (const char of s) {
+
+        count.set(char, (count.get(char) || 0) + 1);
+
+    }
+
+    for (const char of t) {
+
+        if (!count.has(char) || count.get(char) === 0) {
+
+            return false;
+
+        }
+
+        count.set(char, count.get(char) - 1);
+
+    }
+
+    return true;
+
+}
+}
